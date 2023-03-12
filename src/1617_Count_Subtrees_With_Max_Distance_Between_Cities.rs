0897cb76cea0a26cc20c8beb9477ab5c14161568
chenzongyao200127 <2319251792@qq.com>
2023-03-12 21:30:22 +0800

    周赛+每日一题，一共就做出1道，我是FW

---
+++ src/1617_Count_Subtrees_With_Max_Distance_Between_Cities.rs
@@ -0,0 +1,10 @@
+// 1617_Count_Subtrees_With_Max_Distance_Between_Cities
+// https://leetcode.cn/problems/count-subtrees-with-max-distance-between-cities/
+
+
+// 3/12/2023 每日一题
+impl Solution {
+    pub fn count_subgraphs_for_each_diameter(n: i32, edges: Vec<Vec<i32>>) -> Vec<i32> {
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,83 @@
 
 pub fn main() {
-    let ans = trap(vec![0,1,0,2,1,0,1,3,2,1,2,1]);
-    assert_eq!(ans, 6);
+    let ans = count_subgraphs_for_each_diameter(4, vec![vec![1,2],vec![2,3],vec![2,4]]);
+    assert_eq!(ans, vec![3,4,0]);
 }
 
-pub fn trap(height: Vec<i32>) -> i32 {
-    let mut stk: Vec<usize>= vec![];
-    let mut ans = 0;
-
-    for i in 0..height.len() {
-        while !stk.is_empty() && height[stk[stk.len()-1]] < height[i] {
-            let top = stk.pop().unwrap();
-            if stk.is_empty() {
-                break;
-            }
-            let left = stk[stk.len()-1];
-            ans += (i-left-1)*((height[i].min(height[left])-height[top]) as usize);
-        }
-        stk.push(i);
-    }
-
-    ans as i32
+pub fn count_subgraphs_for_each_diameter(n: i32, edges: Vec<Vec<i32>>) -> Vec<i32> {
+    let mut roads = vec![];
+    
 }
 
-pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-    nums.binary_search(&target).unwrap_or_else(|x| x) as i32
-}
+
+// pub fn beautiful_subarrays(nums: Vec<i32>) -> i64 {
+//     if nums.len() <= 1 {
+//         return 0;
+//     }
+//     let mut ans = 0;
+//     for i in 0..nums.len()-1 {
+//         let mut pre_xor = vec![0; 20];
+//         let mut tmp = nums[i];
+//         let mut idx = 0;
+//         while tmp != 0 {
+//             if tmp & 1 == 1 {
+//                 pre_xor[idx] = 1;
+//             }
+//             tmp >>= 1;
+//             idx += 1;
+//         }
+//         for j in i+1..nums.len() {
+//             let mut tmp = nums[j];
+//             let mut idx = 0;
+//             let mut tmp_xor = vec![0; 20];
+//             while tmp != 0 {
+//                 if tmp & 1 == 1 {
+//                     tmp_xor[idx] = 1;
+//                 }
+//                 tmp >>= 1;
+//                 idx += 1;
+//             }
+//             for i in 0..pre_xor.len() {
+//                 pre_xor[i] = pre_xor[i] ^ tmp_xor[i];
+//             }
+//             if pre_xor == vec![0; 20] {
+//                 ans += 1;
+//             }
+//         }
+//     }
+//     ans
+// }
+
+// pub fn max_score(nums: Vec<i32>) -> i32 {
+//     let mut nums = nums;
+//     nums.sort_by_key(|&x| -x.abs());
+//     let mut prefix_sum = 0;
+//     let mut cnt = 0;
+//     for num in &nums {
+//         prefix_sum += num;
+//         if *num > 0 {
+//             cnt += 1;
+//         }
+//     }
+//     if prefix_sum >= 0 {
+//         cnt
+//     } else {
+//         cnt - 1
+//     }
+// }
+
+// pub fn max_score(nums: Vec<i32>) -> i32 {
+//     let mut nums = nums;
+//     let mut ans = 0;
+//     nums.sort_unstable_by(|a, b| b.cmp(&a));
+//     println!("{:?}", nums);
+//     let mut pre_sum = 0;
+//     for i in 0..nums.len() {
+//         pre_sum += nums[i];
+//         if pre_sum > 0 {
+//             ans += 1;
+//         }
+//     }
+
+//     ans
+// }
