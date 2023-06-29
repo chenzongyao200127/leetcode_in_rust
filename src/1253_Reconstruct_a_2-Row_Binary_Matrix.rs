1a312581ad61eba0cc252e972c039fd26c774ff5
chenzongyao200127 <2319251792@qq.com>
2023-06-29 11:05:52 +0800

    每日一题

--- src/1253_Reconstruct_a_2-Row_Binary_Matrix.rs
+++ src/1253_Reconstruct_a_2-Row_Binary_Matrix.rs
@@ -1,58 +1,77 @@
 // 1253_Reconstruct_a_2-Row_Binary_Matrix
 // https://leetcode.cn/problems/reconstruct-a-2-row-binary-matrix/
 
-
+// 贪心
 // 输入：upper = 2, lower = 1, colsum = [1,1,1]
 // 输出：[[1,1,0],[0,0,1]]
 // 解释：[[1,0,1],[0,1,0]] 和 [[0,1,1],[1,0,0]] 也是正确答案。
-
 impl Solution {
     pub fn reconstruct_matrix(upper: i32, lower: i32, colsum: Vec<i32>) -> Vec<Vec<i32>> {
         let mut matrix = vec![vec![0; colsum.len()]; 2];
+        let mut upper_remaining = upper;
+        let mut lower_remaining = lower;
+
         for i in 0..colsum.len() {
             if colsum[i] == 2 {
                 matrix[0][i] = 1;
-                matrix[1][i] = 1;    
+                matrix[1][i] = 1;
+                upper_remaining -= 1;
+                lower_remaining -= 1;
             }
         }
 
-        Self::dfs(0, upper, lower, colsum, matrix);
-        return matrix
-    }
-
-    fn dfs(cur_col: usize, upper: i32, lower: i32, colsum: &Vec<i32>, matrix: &mut Vec<Vec<I32>>) {
-        if cur_col == colsum.len() {
-            if colsum[cur_col] == 1 {
-                colsum[0][cur_col] = 1;
-                if is_valid(upper, lower, matrix) {
-                    return
-                }
-                colsum[0][cur_col] = 0;
-                colsum[1][cur_col] = 0;
-                if is_valid(upper, lower, matrix) {
-                    return
-                }
-            } else {
-                if is_valid(upper, lower, matrix) {
-                    return
+        for i in 0..colsum.len() {
+            if colsum[i] == 1 {
+                if upper_remaining > 0 {
+                    matrix[0][i] = 1;
+                    upper_remaining -= 1;
+                } else {
+                    matrix[1][i] = 1;
+                    lower_remaining -= 1;
                 }
             }
         }
-        for i in 0..=1 {
-            colsum[0][cur_col] = 1;
-            next_col = i+1;
-            while next_col < colsum.len() && colsum[next_col] != 1 {
-                next_col += 1;
-            }
-            dfs(next_col, upper, lower, colsum, matrix);
-            colsum[0][cur_col] = 0;
+
+        if upper_remaining == 0 && lower_remaining == 0 {
+            matrix
+        } else {
+            vec![]
         }
     }
+}
 
-    fn is_valid(upper: i32, lower: i32, matrix: &Vec<Vec<I32>>) -> bool {
-        if matrix[0].iter().sum() == upper && matrix[1].iter().sum() == lower {
-            return true;
+
+
+
+impl Solution {
+    pub fn reconstruct_matrix(upper: i32, lower: i32, colsum: Vec<i32>) -> Vec<Vec<i32>> {
+        let s: i32 = colsum.iter().sum();
+        if s != upper + lower {
+            return vec![];
         }
-        false
+
+        let c1 = colsum.iter().filter(|&a| *a == 2).count() as i32;
+        if c1 > lower || c1 > upper {
+            return vec![];
+        }
+
+        let n = colsum.len();
+        let mut ans = vec![vec![0; n]; 2];
+        let mut diff = upper - lower;
+        for i in 0..n {
+            if colsum[i] == 2 {
+                ans[0][i] = 1;
+                ans[1][i] = 1;
+            } else if colsum[i] == 1 {
+                if diff < 0 {
+                    diff += 1;
+                    ans[1][i] = 1;
+                } else {
+                    diff -= 1;
+                    ans[0][i] = 1;
+                }
+            }
+        }
+        ans
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,85 @@
 
-
 pub fn reconstruct_matrix(upper: i32, lower: i32, colsum: Vec<i32>) -> Vec<Vec<i32>> {
     let mut matrix = vec![vec![0; colsum.len()]; 2];
+    let mut upper_remaining = upper;
+    let mut lower_remaining = lower;
+
     for i in 0..colsum.len() {
         if colsum[i] == 2 {
             matrix[0][i] = 1;
-            matrix[1][i] = 1;    
+            matrix[1][i] = 1;
+            upper_remaining -= 1;
+            lower_remaining -= 1;
         }
     }
 
-    dfs(0, upper, lower, &colsum, &mut matrix);
-    return matrix
-}
-
-pub fn dfs(cur_col: usize, upper: i32, lower: i32, colsum: &Vec<i32>, matrix: &mut Vec<Vec<i32>>) {
-    if cur_col == colsum.len() {
-        if colsum[cur_col] == 1 {
-            matrix[0][cur_col] = 1;
-            if is_valid(upper, lower, matrix) {
-                return
-            }
-            matrix[0][cur_col] = 0;
-            matrix[1][cur_col] = 0;
-            if is_valid(upper, lower, matrix) {
-                return
-            }
-        } else {
-            if is_valid(upper, lower, matrix) {
-                return
+    for i in 0..colsum.len() {
+        if colsum[i] == 1 {
+            if upper_remaining > 0 {
+                matrix[0][i] = 1;
+                upper_remaining -= 1;
+            } else {
+                matrix[1][i] = 1;
+                lower_remaining -= 1;
             }
         }
     }
-    for i in 0..=1 {
-        matrix[0][cur_col] = 1;
-        let mut next_col = i+1;
-        while next_col < colsum.len() && colsum[next_col] != 1 {
-            next_col += 1;
-        }
-        dfs(next_col, upper, lower, colsum, matrix);
-        matrix[0][cur_col] = 0;
+
+    if upper_remaining == 0 && lower_remaining == 0 {
+        matrix
+    } else {
+        vec![]
     }
 }
 
-pub fn is_valid(upper: i32, lower: i32, matrix: &Vec<Vec<i32>>) -> bool {
-    if matrix[0].iter().sum() == upper && matrix[1].iter().sum() == lower {
-        return true;
-    }
-    false
-}
+// pub fn reconstruct_matrix(upper: i32, lower: i32, colsum: Vec<i32>) -> Vec<Vec<i32>> {
+//     let mut matrix = vec![vec![0; colsum.len()]; 2];
+//     for i in 0..colsum.len() {
+//         if colsum[i] == 2 {
+//             matrix[0][i] = 1;
+//             matrix[1][i] = 1;    
+//         }
+//     }
+
+//     dfs(0, upper, lower, &colsum, &mut matrix);
+//     return matrix
+// }
+
+// pub fn dfs(cur_col: usize, upper: i32, lower: i32, colsum: &Vec<i32>, matrix: &mut Vec<Vec<i32>>) {
+//     if cur_col == colsum.len() {
+//         if colsum[cur_col] == 1 {
+//             matrix[0][cur_col] = 1;
+//             if is_valid(upper, lower, matrix) {
+//                 return
+//             }
+//             matrix[0][cur_col] = 0;
+//             matrix[1][cur_col] = 0;
+//             if is_valid(upper, lower, matrix) {
+//                 return
+//             }
+//         } else {
+//             if is_valid(upper, lower, matrix) {
+//                 return
+//             }
+//         }
+//     }
+//     for i in 0..=1 {
+//         matrix[0][cur_col] = 1;
+//         let mut next_col = i+1;
+//         while next_col < colsum.len() && colsum[next_col] != 1 {
+//             next_col += 1;
+//         }
+//         dfs(next_col, upper, lower, colsum, matrix);
+//         matrix[0][cur_col] = 0;
+//     }
+// }
+
+// pub fn is_valid(upper: i32, lower: i32, matrix: &Vec<Vec<i32>>) -> bool {
+//     if matrix[0].iter().sum() == upper && matrix[1].iter().sum() == lower {
+//         return true;
+//     }
+//     false
+// }
 
 
 fn main() {
