027c15d0205fb722eaf120c6f256161a6465699c
chenzongyao200127 <2319251792@qq.com>
2023-07-27 09:36:34 +0800

    每日一题

---
+++ src/2500_Delete_Greatest_Value_in_Each_Row.go
@@ -0,0 +1,18 @@
+// 2500. Delete Greatest Value in Each Row
+// https://leetcode.cn/problems/delete-greatest-value-in-each-row/description/
+
+func deleteGreatestValue(grid [][]int) (ans int) {
+	for _, row := range grid {
+		sort.Ints(row)
+	}
+	for j := range grid[0] {
+		t := 0
+		for i := range grid {
+			if t < grid[i][j] {
+				t = grid[i][j]
+			}
+		}
+		ans += t
+	}
+	return
+}
