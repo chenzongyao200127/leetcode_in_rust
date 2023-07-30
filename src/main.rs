5c08d7b177ad9ae29c70a6ab17662b9b5afdbaa1
chenzongyao200127 <2319251792@qq.com>
2023-07-30 19:00:15 +0800

    DP + 每日一题

--- src/main.rs
+++ src/main.rs
@@ -96,3 +96,17 @@ fn main() {
     assert_eq!(ans, 4);
 }
 
+pub fn num_trees(n: i32) -> i32 {
+    let n = n as usize;
+    let mut dp = vec![0; n + 1];
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for i in 2..=n {
+        for j in 1..i {
+            dp[i] += dp[j-1] * dp[i-j];
+        }
+    }
+
+    dp[n]
+}
