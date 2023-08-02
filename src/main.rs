10569b1ae00b0d4b4e3d3eeaf52e37fc9b7c4799
chenzongyao200127 <2319251792@qq.com>
2023-08-03 00:04:36 +0800

    DP

--- src/main.rs
+++ src/main.rs
@@ -238,3 +238,30 @@ pub fn longest_increasing_path(matrix: Vec<Vec<i32>>) -> i32 {
 
     ans
 }
+
+
+pub fn longest_palindrome_subseq(s: String) -> i32 {
+    let s1 = s.clone();
+    let s2 = s.chars().rev().collect::<String>();
+
+    #[inline]
+    fn longest_common_subsequence(s1: &str, s2: &str) -> i32 {
+        let s1_chars: Vec<char> = s1.chars().collect();
+        let s2_chars: Vec<char> = s2.chars().collect();
+
+        let mut dp = vec![vec![0; s1_chars.len() + 1]; s2_chars.len() + 1];
+        for i in 1..=s2_chars.len() {
+            for j in 1..=s1_chars.len() {
+                if s2_chars[i - 1] == s1_chars[j - 1] {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j].max(dp[i][j - 1])
+                }
+            }
+        }
+
+        dp[s2_chars.len()][s1_chars.len()]
+    }
+
+    longest_common_subsequence(&s1, &s2)
+}
