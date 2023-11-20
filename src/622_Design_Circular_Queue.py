293a30ccb7e0970b3081d72ecdd190358a648d21
chenzongyao200127 <2319251792@qq.com>
2023-11-20 10:07:05 +0800

    ...

---
+++ src/622_Design_Circular_Queue.py
@@ -0,0 +1,70 @@
+# 622_Design_Circular_Queue
+# https://leetcode.cn/problems/design-circular-queue/
+
+class Node:
+    def __init__(self, v: int):
+        self.pre = None
+        self.nxt = None
+        self.val = v
+
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.cnt = 0
+        self.capacity = k
+        self.dummy = Node(0)
+        self.dummy.nxt = self.dummy
+        self.dummy.pre = self.dummy
+        
+
+    def enQueue(self, value: int) -> bool:
+        if self.cnt == self.capacity:
+            return False
+        
+        node = Node(value)
+        last_node = self.dummy.pre
+        last_node.nxt = node
+        node.pre = last_node
+        node.nxt = self.dummy
+        self.dummy.pre = node
+        
+        self.cnt += 1
+        
+        return True
+
+    def deQueue(self) -> bool:
+        if self.cnt == 0:
+            return False
+
+        second_node = self.dummy.nxt.nxt
+        self.dummy = second_node
+        second_node.pre = self.dummy
+        
+        self.cnt -= 1
+         
+        return True
+
+    def Front(self) -> int:
+        return self.dummy.nxt.val
+
+
+    def Rear(self) -> int:
+        return self.dummy.pre.val
+
+
+    def isEmpty(self) -> bool:
+        return self.cnt == 0
+
+    def isFull(self) -> bool:
+        return self.cnt == self.capacity
+
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
--- src/main.rs
+++ src/main.rs
@@ -461,10 +461,56 @@ pub fn find_peak_element(nums: Vec<i32>) -> i32 {
     l as i32
 }
 
-pub fn max_sum_of_three_subarrays(nums: Vec<i32>, k: i32) -> Vec<i32> {
+struct MyCircularQueue {
 
 }
 
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl MyCircularQueue {
+
+    fn new(k: i32) -> Self {
+
+    }
+    
+    fn en_queue(&self, value: i32) -> bool {
+
+    }
+    
+    fn de_queue(&self) -> bool {
+
+    }
+    
+    fn front(&self) -> i32 {
+
+    }
+    
+    fn rear(&self) -> i32 {
+
+    }
+    
+    fn is_empty(&self) -> bool {
+
+    }
+    
+    fn is_full(&self) -> bool {
+
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * let obj = MyCircularQueue::new(k);
+ * let ret_1: bool = obj.en_queue(value);
+ * let ret_2: bool = obj.de_queue();
+ * let ret_3: i32 = obj.front();
+ * let ret_4: i32 = obj.rear();
+ * let ret_5: bool = obj.is_empty();
+ * let ret_6: bool = obj.is_full();
+ */
 
 fn main() {
     // let ans = find_closest_elements(vec![1,2,3,4,5], 4, 3);
