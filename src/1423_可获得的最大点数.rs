11a1a52cb43f81a912c8a6a3722c165a501ff0d6
chenzongyao200127 <2319251792@qq.com>
2023-12-03 17:21:24 +0800

    补充解法

--- src/1423_可获得的最大点数.rs
+++ src/1423_可获得的最大点数.rs
@@ -49,3 +49,17 @@ impl Solution {
         max_score
     }
 }
+
+
+impl Solution {
+    pub fn max_score(card_points: Vec<i32>, k: i32) -> i32 {
+        let k = k as usize;
+        let mut s = card_points.iter().take(k).sum::<i32>();
+        let mut ans = s;
+        for i in 1..=k {
+            s += card_points[card_points.len() - i] - card_points[k - i];
+            ans = ans.max(s);
+        }
+        ans
+    }
+}
