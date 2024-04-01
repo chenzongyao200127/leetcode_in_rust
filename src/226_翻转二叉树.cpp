36bd5899a4c81fdff541474b6604eef8217c570c
chenzongyao200127 <2319251792@qq.com>
2024-04-01 22:40:17 +0800

    update

---
+++ src/226_翻转二叉树.cpp
@@ -0,0 +1,36 @@
+// 226_翻转二叉树
+// https://leetcode.cn/problems/invert-binary-tree/description/?envType=study-plan-v2&envId=top-100-liked
+
+#include <vector>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return nullptr;
+        }
+        TreeNode *left = invertTree(root->left);
+        TreeNode *right = invertTree(root->right);
+        root->left = right;
+        root->right = left;
+        return root;
+    }
+};
