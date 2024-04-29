d33177f795a5ad1a343b76d99a915a1ecb048294
chenzongyao200127 <2319251792@qq.com>
2024-04-29 13:54:24 +0800

    update

--- src/35_Search_Insert_Position.py
+++ src/35_Search_Insert_Position.py
@@ -3,6 +3,7 @@
 
 from typing import List
 
+
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         l = 0
@@ -17,7 +18,7 @@ class Solution:
             if mid - l == 1 or r - mid == 1:
                 return l + 1
         return mid
-    
+
 # There are a few issues with your binary search logic:
 
 # 1. You initialize `r` to `len(nums)`, but it should be `len(nums) - 1` since list indices are 0-based.
@@ -25,6 +26,7 @@ class Solution:
 # 3. In the condition where `nums[mid] < target`, you should move the left boundary `l`, not the right boundary `r`. Conversely, when `nums[mid] > target`, you should move the right boundary `r`, not the left boundary `l`.
 # 4. The check `if mid - l == 1 or r - mid == 1` is not very intuitive. Instead, a better stopping condition would be `while l <= r`.
 # 5. After the loop finishes, if the target is not found, we can return the `l` index as the position where `target` should be inserted.
+
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,3 @@
-// 1017. 二进制转换
-pub fn base_2(n: i32) -> String {
-    let mut res = String::new();
-    let mut n = n;
-    while n > 0 {
-        res.push_str(&(n % 2).to_string());
-        n /= 2;
-    }
-    res.chars().rev().collect()
-}
-
-pub fn base_neg2(n: i32) -> String {
-    let n_base2 = base_2(n);
-    let mut res = String::new();
-    let len = n_base2.len();
-    let mut carry = 0;
-    println!("{:?}", n_base2);
-    for i in (0..len).rev() {
-        println!("carry: {:?}", carry);
-        let mut num = n_base2.chars().nth(i).unwrap().to_digit(10).unwrap() + carry;
-        println!("num {:?}", num);
-        if (len - i) % 2 == 1 {
-            num += 2;
-        }
-        carry = num / 2;
-        res.push_str(&(num % 2).to_string());
-    }
-    if carry > 0 {
-        res.push_str(&carry.to_string());
-    }
-    res.chars().rev().collect()
-}
-
 // 1329_将矩阵按对角线排序
 pub fn diagonal_sort(mat: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     let mut mat = mat;
@@ -81,15 +48,6 @@ pub fn diagonal_sort(mat: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     mat
 }
 
-/**
- * Your SnapshotArray object will be instantiated and called as such:
- * let obj = SnapshotArray::new(length);
- * obj.set(index, val);
- * let ret_2: i32 = obj.snap();
- * let ret_3: i32 = obj.get(index, snap_id);
- */
-
 fn main() {
-    let res = base_neg2(3);
-    println!("{:?}", res);
+    println!("Hello, world!")
 }
