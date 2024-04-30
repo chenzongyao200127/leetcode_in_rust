6c8599420dcad89701cc059c96ed77a92a3a9d7c
chenzongyao200127 <2319251792@qq.com>
2024-04-30 10:42:18 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,21 @@
-// 1329_将矩阵按对角线排序
-pub fn diagonal_sort(mat: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    let mut mat = mat;
-    let m = mat.len();
-    let n = mat[0].len();
+use std::marker::PhantomData;
 
-    for i in 0..m {
-        let mut temp = Vec::new();
-        let mut x = i;
-        let mut y = 0;
-        while x < m && y < n {
-            temp.push(mat[x][y]);
-            x += 1;
-            y += 1;
-        }
-        temp.sort();
-        let mut x = i;
-        let mut y = 0;
-        let mut index = 0;
-        while x < m && y < n {
-            mat[x][y] = temp[index];
-            x += 1;
-            y += 1;
-            index += 1;
-        }
-    }
+struct Bounded<'a, 'b: 'a, T: ?Sized>(&'a T, PhantomData<&'b ()>);
 
-    for i in 1..n {
-        let mut temp = Vec::new();
-        let mut x = 0;
-        let mut y = i;
-        while x < m && y < n {
-            temp.push(mat[x][y]);
-            x += 1;
-            y += 1;
-        }
-        temp.sort();
-        let mut x = 0;
-        let mut y = i;
-        let mut index = 0;
-        while x < m && y < n {
-            mat[x][y] = temp[index];
-            x += 1;
-            y += 1;
-            index += 1;
-        }
-    }
-    mat
+fn helper<'a, 'b, T: ?Sized, F>(input: &'a T, closure: F) -> &'b T
+where
+    F: for<'c> FnOnce(&'c T) -> Bounded<'b, '_, T>,
+{
+    closure(input).0
+}
+
+fn extend<'a, 'b, T: ?Sized>(input: &'a T) -> &'b T {
+    helper(input, |x| Bounded(x, PhantomData))
 }
 
 fn main() {
-    println!("Hello, world!")
+    let s = String::from("aaaa");
+    let a: &'static str = extend(s.as_str()); // turn &'a str into 'static
+    drop(s);
+    println!("{a}"); // <------------ Use after free!
 }
