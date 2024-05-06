65637c3e4494ffad708bffe9eea27c1ad38cc708
chenzongyao200127 <2319251792@qq.com>
2024-05-06 20:48:32 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,22 @@ struct Bounded<'a, 'b: 'a, T: ?Sized>(&'a T, PhantomData<&'b ()>);
 
 fn helper<'a, 'b, T: ?Sized, F>(input: &'a T, closure: F) -> &'b T
 where
+    'a: 'b,
     F: for<'c> FnOnce(&'c T) -> Bounded<'b, '_, T>,
 {
     closure(input).0
 }
 
-fn extend<'a, 'b, T: ?Sized>(input: &'a T) -> &'b T {
+fn extend<'a, 'b, T: ?Sized>(input: &'a T) -> &'b T
+where
+    'a: 'b,
+{
     helper(input, |x| Bounded(x, PhantomData))
 }
 
 fn main() {
     let s = String::from("aaaa");
-    let a: &'static str = extend(s.as_str()); // turn &'a str into 'static
-    drop(s);
-    println!("{a}"); // <------------ Use after free!
+    // turn &'a str into 'static
+    let s: &'static str = extend(&s);
+    println!("{}", s);
 }
